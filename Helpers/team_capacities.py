9c4f459aff212bcc9c22daba74d5b10b54e74f68
P4Bs <pablojesus.mp@gmail.com>
2026-03-09 19:38:53 +0100

    Calculate team T, S and K coverage and deviation from target team

--- Helpers/team_capacities.py
+++ Helpers/team_capacities.py
@@ -1,27 +1,11 @@
-import re
+from typing import Tuple
+from Models.team import Team
 
-from Models.initial_team import Team
-from Models.relationship import Relationship
-from Services.nice_service import load_nice_framework_relationships
 
-TASK_REGEX = re.compile("^T\\d{4}$")
-SKILLS_REGEX = re.compile("^S\\d{4}$")
-KNOWLEDGE_REGEX = re.compile("^K\\d{4}$")
-
-def extract_team_tasks_skills_knowledge(team_roles: list[str] = ["DD-WRL-002"]) -> tuple[list[str], list[str], list[str]]:
-    team_tasks: list[str] = []
-    team_skills: list[str] = []
-    team_knowledge: list[str] = []
-    nice_framework_relationships: list[Relationship] = load_nice_framework_relationships()
-    for role in team_roles:
-        capacities_filtered_by_role: list[str] = [relationship["dest_element_identifier"] for relationship in nice_framework_relationships if relationship["source_element_identifier"] == role]
-
-        team_tasks += filter(TASK_REGEX.match, capacities_filtered_by_role)
-        team_skills += filter(SKILLS_REGEX.match, capacities_filtered_by_role)
-        team_knowledge += filter(KNOWLEDGE_REGEX.match, capacities_filtered_by_role)
-
-    return team_tasks, team_skills, team_knowledge
-
-# TODO: WRITE THIS FUNCTION
-def compare_teams(current_team: Team, target_team: Team):
-    print("")
+def compare_teams(current_team: Team, target_team: Team) -> Tuple[float, set[str], set[str]]:
+    preparation_ratio: float = 1.0
+    skillset_match_set: set[str] = current_team.complete_skillset.intersection(target_team.complete_skillset)
+    skillset_diff_set: set[str] = target_team.complete_skillset.difference(current_team.complete_skillset)
+    if len(skillset_match_set) < len(target_team.complete_skillset):
+        preparation_ratio = len(skillset_match_set) / len(target_team.complete_skillset)
+    return preparation_ratio, skillset_match_set, skillset_diff_set
