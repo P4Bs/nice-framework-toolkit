ed03ed3dc28c8577db69d15f125ca75270b7c1a5
Francisco Velasco Romero <velascoromerofrancisco@gmail.com>
2026-03-10 01:45:25 +0100

    [Perf]: Complejidad de extract_roles_capacities reducido de O(R * N) a O(N + R) || NiceService pasa a ser Singleton

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from Helpers.team_capacities import compare_teams
 from Models.team import Team
+from Services.nice_service import NiceService
 
 
 def main():
-    print("Hello World")
+    print("Instanciando el servicio NICE...")
+    nice_service = NiceService()
+    print("Servicio NICE instanciado correctamente.")
     current_team = Team(["IO-WRL-005", "PD-WRL-006"])
     target_team = Team(["PD-WRL-001"])
     coverage, matched_skills, difference_skill = compare_teams(current_team, target_team)
